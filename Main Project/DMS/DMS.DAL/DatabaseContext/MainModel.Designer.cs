e6601f0e1c55e5c3321c77f102b5397a84eb6bb9
romiyadangol <dangolromiya@gmail.com>
2022-04-19 09:13:55 +0545

    new changes

--- Main Project/DMS/DMS.DAL/DatabaseContext/MainModel.Designer.cs
+++ Main Project/DMS/DMS.DAL/DatabaseContext/MainModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\4th sem\asp\WebApplication2\Main Project\DMS\DMS.DAL\DatabaseContext\MainModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\4th sem project\Main Project\DMS\DMS.DAL\DatabaseContext\MainModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
